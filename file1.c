88563f58987f2cf05fd01dc464768795d0a8c61d
MostafaSabryEE <mostafa.sabrry@outlook.com.com>
2023-03-17 16:32:40 +0200

    Version 1

---
+++ file1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	printf("Hello Mostafa.\n");
+	printf("%d",2+2);
+	return 0;
+}
--- main.c
+++ main.c
@@ -3,5 +3,6 @@ int main()
 {
 	printf("Hello Mostafa.\n");
 	printf("Hello To The New GIT Repo.\n");
+	printf("%d",3+3);
 	return 0;
 }
